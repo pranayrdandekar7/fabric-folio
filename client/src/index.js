74476636fd93d1b7f122e7632ff47912918bfdf6
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-12-04 17:49:31 +0530

    changes innbackend

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {createBrowserRouter,RouterProvider} from "react-router-dom"
+import Home from './views/Home/Home';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path :"/",
+    element:<Home/>
+  },
+  {
+    path:"*",
+    element:<h1>404 Not Found</h1>
+  }
+])
+root.render(<RouterProvider router={router}/>);
+
