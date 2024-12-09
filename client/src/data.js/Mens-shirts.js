b0699bbd84cae32571d6379757dbe00d514a1f13
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-12-09 15:39:05 +0530

    modification in data file

--- client/src/data.js/Mens-shirts.js
+++ client/src/data.js/Mens-shirts.js
@@ -1,6 +1,6 @@
 const MENS_SHIRTS = [
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmAq2z7ENfuJvxlKlMgfEXnFL10QaR_S7zgQ&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8ScwIc7PXuL6gieUTJ55XlAZoMUDYNyZqwA&s",
         "brand": "Urban Style",
         "title": "Men Slim Fit Formal Shirt",
         "color": "White",
@@ -8,10 +8,22 @@ const MENS_SHIRTS = [
         "discountPrice": 749,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 15 },
-            { "name": "M", "quantity": 20 },
-            { "name": "L", "quantity": 25 },
-            { "name": "XL", "quantity": 10 }
+            {
+                "name": "S",
+                "quantity": 15
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 25
+            },
+            {
+                "name": "XL",
+                "quantity": 10
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -19,7 +31,7 @@ const MENS_SHIRTS = [
         "description": "Classic white slim-fit formal shirt for office and occasions."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5tKNxJ3PVH90QHsd4iwF9bPX9mRjtl5p0nQ&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnwOzhFTiE1tF_xDrSYd8wyPaFLgH1Y0MfLQ&s",
         "brand": "Trend Setter",
         "title": "Men Checked Casual Shirt",
         "color": "Red",
@@ -27,10 +39,22 @@ const MENS_SHIRTS = [
         "discountPrice": 649,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 10 },
-            { "name": "M", "quantity": 15 },
-            { "name": "L", "quantity": 20 },
-            { "name": "XL", "quantity": 8 }
+            {
+                "name": "S",
+                "quantity": 10
+            },
+            {
+                "name": "M",
+                "quantity": 15
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+            {
+                "name": "XL",
+                "quantity": 8
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -38,7 +62,7 @@ const MENS_SHIRTS = [
         "description": "Red checked shirt for a casual and trendy look."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFd6szksC7A9NmNXkzt3TFz2nIg_V3v7PzxA&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHXFzWbEdyZeUHsTUZTsn6W1L8yVSWWuBkzw&s",
         "brand": "Classic Fit",
         "title": "Men Solid Cotton Shirt",
         "color": "Blue",
@@ -46,10 +70,22 @@ const MENS_SHIRTS = [
         "discountPrice": 699,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 18 },
-            { "name": "M", "quantity": 25 },
-            { "name": "L", "quantity": 30 },
-            { "name": "XL", "quantity": 12 }
+            {
+                "name": "S",
+                "quantity": 18
+            },
+            {
+                "name": "M",
+                "quantity": 25
+            },
+            {
+                "name": "L",
+                "quantity": 30
+            },
+            {
+                "name": "XL",
+                "quantity": 12
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -57,7 +93,7 @@ const MENS_SHIRTS = [
         "description": "Comfortable solid blue shirt made of 100% cotton."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSF2eG92yghG6Adik0XvJgpdIADxMcPlRzzA&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2Xx2Pg0v2IMVWvjq6bIXCtWp8b9mkto8YEg&s",
         "brand": "Rugged Wear",
         "title": "Men Printed Casual Shirt",
         "color": "Green",
@@ -65,10 +101,22 @@ const MENS_SHIRTS = [
         "discountPrice": 799,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 15 },
-            { "name": "M", "quantity": 20 },
-            { "name": "L", "quantity": 18 },
-            { "name": "XL", "quantity": 8 }
+            {
+                "name": "S",
+                "quantity": 15
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 18
+            },
+            {
+                "name": "XL",
+                "quantity": 8
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -76,7 +124,7 @@ const MENS_SHIRTS = [
         "description": "Stylish printed shirt with vibrant green patterns."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjKYq3tG9DEyb3IGicXNdW2-HQ_dJeRDzvDA&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3-BDJCz3vlvfNhhBGKgFE1ax8RocTUNuGDg&s",
         "brand": "Formal Edge",
         "title": "Men Regular Fit Striped Shirt",
         "color": "Grey",
@@ -84,10 +132,22 @@ const MENS_SHIRTS = [
         "discountPrice": 899,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 8 },
-            { "name": "M", "quantity": 15 },
-            { "name": "L", "quantity": 20 },
-            { "name": "XL", "quantity": 10 }
+            {
+                "name": "S",
+                "quantity": 8
+            },
+            {
+                "name": "M",
+                "quantity": 15
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+            {
+                "name": "XL",
+                "quantity": 10
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -95,7 +155,7 @@ const MENS_SHIRTS = [
         "description": "Grey striped shirt for a polished and professional appearance."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTET4ysLoLooxGNEyC_X_NC1-LJSFONPe_LHg&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRz5RRLIPmg3G0V7xkth3vg6ZjVX1JsJxYK1w&s",
         "brand": "Urban Edge",
         "title": "Men Linen Blend Casual Shirt",
         "color": "Beige",
@@ -103,10 +163,22 @@ const MENS_SHIRTS = [
         "discountPrice": 749,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 12 },
-            { "name": "M", "quantity": 18 },
-            { "name": "L", "quantity": 20 },
-            { "name": "XL", "quantity": 8 }
+            {
+                "name": "S",
+                "quantity": 12
+            },
+            {
+                "name": "M",
+                "quantity": 18
+            },
+            {
+                "name": "L",
+                "quantity": 20
+            },
+            {
+                "name": "XL",
+                "quantity": 8
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -114,7 +186,7 @@ const MENS_SHIRTS = [
         "description": "Lightweight linen-blend shirt for a comfortable and casual style."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpeu7XtUXR9iy_OLXTjdfDuFI_XCm8LBYz0A&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTld7MwZiIHurGFlIzbelLHPFu_sF1eweAJsg&s",
         "brand": "Denim Touch",
         "title": "Men Denim Shirt",
         "color": "Dark Blue",
@@ -122,10 +194,22 @@ const MENS_SHIRTS = [
         "discountPrice": 999,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 10 },
-            { "name": "M", "quantity": 15 },
-            { "name": "L", "quantity": 25 },
-            { "name": "XL", "quantity": 8 }
+            {
+                "name": "S",
+                "quantity": 10
+            },
+            {
+                "name": "M",
+                "quantity": 15
+            },
+            {
+                "name": "L",
+                "quantity": 25
+            },
+            {
+                "name": "XL",
+                "quantity": 8
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -133,7 +217,7 @@ const MENS_SHIRTS = [
         "description": "Dark blue denim shirt for a rugged and casual look."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ9tmW8K53OPLvDs7rOtwfU7ux6k8_7aGUzg&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4GotCLXySpRhOOcpHYE3luakae1X7k24_ug&s",
         "brand": "Bold Stitch",
         "title": "Men Short Sleeve Printed Shirt",
         "color": "Yellow",
@@ -141,10 +225,22 @@ const MENS_SHIRTS = [
         "discountPrice": 649,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 15 },
-            { "name": "M", "quantity": 20 },
-            { "name": "L", "quantity": 15 },
-            { "name": "XL", "quantity": 5 }
+            {
+                "name": "S",
+                "quantity": 15
+            },
+            {
+                "name": "M",
+                "quantity": 20
+            },
+            {
+                "name": "L",
+                "quantity": 15
+            },
+            {
+                "name": "XL",
+                "quantity": 5
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -152,7 +248,7 @@ const MENS_SHIRTS = [
         "description": "Bright yellow printed shirt with short sleeves for a summer vibe."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsyR5tpw-2M8utbhTRTmbkC4ciWd1N56zHYA&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxE3qFBW7EmD_wDzGX3bB0_NzaUqdXfbdUEQ&s",
         "brand": "Style Fit",
         "title": "Men Slim Fit Mandarin Collar Shirt",
         "color": "Black",
@@ -160,10 +256,22 @@ const MENS_SHIRTS = [
         "discountPrice": 799,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 20 },
-            { "name": "M", "quantity": 25 },
-            { "name": "L", "quantity": 30 },
-            { "name": "XL", "quantity": 15 }
+            {
+                "name": "S",
+                "quantity": 20
+            },
+            {
+                "name": "M",
+                "quantity": 25
+            },
+            {
+                "name": "L",
+                "quantity": 30
+            },
+            {
+                "name": "XL",
+                "quantity": 15
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
@@ -171,7 +279,7 @@ const MENS_SHIRTS = [
         "description": "Black mandarin collar shirt for a sleek and sophisticated style."
     },
     {
-        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTg_o5ux2m28GchT5skkIzHzLLSZPjFsJXnkQ&s",
+        "imgUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmBkjpiE1hqMzqUp-PScv8YP1-kgg9nt8ANw&s",
         "brand": "Elite Wear",
         "title": "Men Full Sleeve Checked Shirt",
         "color": "Green",
@@ -179,10 +287,22 @@ const MENS_SHIRTS = [
         "discountPrice": 849,
         "discountPercent": 50,
         "size": [
-            { "name": "S", "quantity": 18 },
-            { "name": "M", "quantity": 22 },
-            { "name": "L", "quantity": 28 },
-            { "name": "XL", "quantity": 10 }
+            {
+                "name": "S",
+                "quantity": 18
+            },
+            {
+                "name": "M",
+                "quantity": 22
+            },
+            {
+                "name": "L",
+                "quantity": 28
+            },
+            {
+                "name": "XL",
+                "quantity": 10
+            }
         ],
         "topLevelCategory": "Men",
         "secondLevelCategory": "Clothing",
