e0205f456dfc3cf329c33a293d711568f73dd905
pranayrdandekar7 <pranayrdandekar7@gmail.com>
2024-12-11 18:47:13 +0530

    some changes done in product card

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -5,12 +5,12 @@ const ProductCard = ({product}) => {
   return (
     <div className=' productCard w-[15rem] m-3 transition-all cursor-pointer rounded '>
       <div className='h-[18 rem] w-full object-cover '>
-        <img src={product.imgUrl} className=' w-full rounded ' alt='product-image' />
+        <img src={product.imgUrl} className=' w-full  h-[16rem] rounded  object-cover object-top' alt='product-image' />
       </div>
       <div className='textpart p-1'>
         <div>
-          <p className='font-bold opacity-80'>Bold Stitch</p>
-          <p>Men Full Sleeve Checked Shirt</p>
+          <p className='font-bold opacity-80'>{product.brand}</p>
+          <p>{product.description.substring(0,55)} . . .</p>
         </div>
 
         <div className='flex  items-center space-x-2'>
